ac1333add51479d2a862afcbb3428a2849e0109f
junyeongpark5 <junyeong5347@gmail.com>
2024-03-27 15:05:17 +0900

    Feat : create navigation bar

--- app/src/main/java/com/example/a3gradesmartcontent/MainActivity.kt
+++ app/src/main/java/com/example/a3gradesmartcontent/MainActivity.kt
@@ -1,15 +1,39 @@
 package com.example.a3gradesmartcontent
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.a3gradesmartcontent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // 첫 번째 프래그먼트로 ChatGPTFragment를 설정합니다.
+        supportFragmentManager.beginTransaction().add(binding.frameContainer.id, ChatgptFragment()).commit()
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_community -> {
+                    replaceFragment(CommunityFragment())
+                }
+                R.id.navigation_chatgpt -> {
+                    replaceFragment(ChatgptFragment())
+                }
+                R.id.navigation_job -> {
+                    replaceFragment(JobsFragment())
+                }
+            }
+            true // 아이템 선택 처리가 완료되었음을 의미합니다.
+        }
+    }
+
+    // 프래그먼트 교체를 위한 함수입니다.
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(binding.frameContainer.id, fragment).commit()
     }
 }
